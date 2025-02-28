d3f6d2517acec0c9c41f3bb331df0bf7bea3e4cf
Jean Dupont <monadresse@example.com>
2025-02-28 23:46:26 +0100

    ajout medecin pour secretaire

--- frontend/src/pages/secretaire/js/dashboard.js
+++ frontend/src/pages/secretaire/js/dashboard.js
@@ -1,5 +1,49 @@
-import { handleNotifications } from "../../../store/notification.js";
+import { getAppointments } from "../../../services/appointmentService.js";
+import { getDoctors } from "../../../services/doctorService.js";
+import { getPatients } from "../../../services/patientService.js";
+import { getCurrentUser } from "../../../store/auth.js";
 
-document.addEventListener("DOMContentLoaded",()=>{
-    handleNotifications();
-})
+document.addEventListener("DOMContentLoaded", async () => {
+  displaySecretaireInfo();
+  await loadDashboardData();
+  const sidebarDeviceButton = document.getElementById("sidebar-device");
+  const sidebarClose = document.getElementById("sidebar-close");
+  sidebarDeviceButton.addEventListener("click", openSidebar);
+  sidebarClose.addEventListener("click", closeSidebar);
+});
+
+export async function loadDashboardData() {
+  const patients = await getPatients();
+  const doctors = await getDoctors();
+  const appointments = await getAppointments();
+
+  document.getElementById("totalPatients").textContent = patients.length;
+  document.getElementById("totalDoctors").textContent = doctors.length;
+  document.getElementById("totalAppointments").textContent =
+    appointments.length;
+}
+
+function displaySecretaireInfo() {
+  const user = getCurrentUser();
+  if (user.role === "secretaire") {
+    const profilePhoto = document.getElementById("profilePhoto");
+    const profileName = document.getElementById("profileName");
+    const profileEmail = document.getElementById("profileEmail");
+    const profileTitle = document.getElementById("profileTitle");
+
+    profilePhoto.src = user.avatar;
+    profileName.textContent = user.prenom;
+    profileEmail.textContent = user.email;
+    profileTitle.textContent = `Bienvenue ${user.prenom} ${user.nom} 👋`;
+  }
+}
+
+function openSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.remove("-translate-x-full");
+}
+
+function closeSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.add("-translate-x-full");
+}
---
+++ frontend/src/pages/secretaire/js/sec_docteur.js
@@ -0,0 +1,84 @@
+import { closeAddDocteurModal, handleAddDocteurFormSubmit, openAddDocteurModal } from "../../../components/modals/docteurs/docteur_modal.js";
+import { getDoctors } from "../../../services/doctorService.js";
+
+
+  
+  document.addEventListener("DOMContentLoaded", async () => {
+    await loadDoctorsTable();
+    await loadModal();
+    const sidebarDeviceButton = document.getElementById("sidebar-device");
+    const sidebarClose = document.getElementById("sidebar-close");
+    sidebarDeviceButton.addEventListener("click", openSidebar);
+    sidebarClose.addEventListener("click", closeSidebar);
+  });
+  
+  async function loadDoctorsTable() {
+    try {
+      const doctors = await getDoctors();
+      const tableBody = document.getElementById("doctorsTableBody");
+  
+      tableBody.innerHTML = "";
+  
+      doctors.forEach((doctor) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+                  <td class="py-2 px-4"><img src="${doctor.avatar}" class="w-10 h-10 rounded-full object-cover"/></td>
+                  <td class="py-2 px-4">${doctor.nom}</td>
+                  <td class="py-2 px-4">${doctor.email}</td>
+                  <td class="py-2 px-4">${doctor.specialite}</td>
+                  <td class="py-2 px-4">
+                      <button class="bg-gray-100 py-1 px-3 rounded-md hover:bg-gray-200 cursor-not-allowed" disabled>
+                      <span>Modifier</span>
+                      <i class="ri-edit-box-line"></i>
+                      </button>
+                      <button class="bg-gray-100 py-1 px-3 rounded-md hover:bg-gray-200 cursor-not-allowed" disabled>
+                      <span>Supprimer</span>
+                      <i class="ri-delete-bin-6-line"></i>
+                      </button>
+                  </td>
+              `;
+        tableBody.appendChild(row);
+      });
+    } catch (error) {
+      console.error("Erreur lors du chargement des docteurs :", error);
+      alert("Une erreur s'est produite lors du chargement des docteurs.");
+    }
+  }
+  
+  async function loadModal() {
+    try {
+      const response = await fetch(
+        "/frontend/src/components/modals/docteurs/docteur_modal.html"
+      );
+      if (!response.ok) {
+        throw new Error("Erreur lors du chargement de la modale");
+      }
+  
+      const modalHTML = await response.text();
+      const modalContainer = document.getElementById("modalContainer");
+      modalContainer.innerHTML = modalHTML;
+      const openModalButton = document.getElementById("openAddDocteurModal");
+      const cancelAddDocteurButton = document.getElementById("cancelAddDocteur");
+      openModalButton.addEventListener("click", openAddDocteurModal);
+      cancelAddDocteurButton.addEventListener("click", closeAddDocteurModal);
+      const form = document.getElementById("addDocteurForm");
+  
+      form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        await handleAddDocteurFormSubmit();
+      });
+    } catch (error) {
+      console.error("Erreur :", error);
+    }
+  }
+  
+  function openSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.remove("-translate-x-full");
+  }
+  
+  function closeSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.add("-translate-x-full");
+  }
+  
